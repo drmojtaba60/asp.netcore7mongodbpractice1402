c693acde918e5fa49a3b04f8556b4056e5a4b0b2
shaghi <mojtaba.shagi@gmail.com>
2023-09-10 18:25:41 +0330

    mongodb

---
+++ asp.netcore7mongodb/Entities/IOptions/MoviesDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace asp.netcore7mongodb.Entities.IOptions
+{
+    public class MoviesDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string MoviesCollectionName { get; set; } = string.Empty;
+    }
+
+}
